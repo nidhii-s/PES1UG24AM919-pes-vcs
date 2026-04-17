258590350171ed36ce7355706eaa58199790d4e5
nidhi <nidhis7mys@gmail.com>
2026-04-17 20:12:03 +0530

    added hashing

--- object.c
+++ object.c
@@ -7,7 +7,7 @@
 //
 // PROVIDED functions: compute_hash, object_path, object_exists, hash_to_hex, hex_to_hash
 // TODO functions:     object_write, object_read
-#include <openssl/sha.h>
+#include <openssl/sha.h>   
 #include "pes.h"
 #include <stdio.h>
 #include <stdlib.h>
